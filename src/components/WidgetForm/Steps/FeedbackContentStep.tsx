499a378a51e438d6fb77aae5a2397a68bf2b2c15
João Paulo <jpfricks@gmail.com>
2022-05-17 22:35:07 -0300

    feat: Create the static form content 🍩

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,6 @@
 import { ButtonClose } from '../../ButtonClose';
 import { FeedbackType, feedbackTypes } from '..';
-import { ArrowLeft } from 'phosphor-react';
+import { ArrowLeft, Camera } from 'phosphor-react';
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
@@ -31,7 +31,27 @@ export function FeedbackContentStep(props: FeedbackContentStepProps) {
         <ButtonClose />
       </header>
 
-      <div className="flex py-8 gap-2 w-full"></div>
+      <form className="my-4 w-full">
+        <textarea
+          className="w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border border-zinc-600 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none rounded-md bg-transparent scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+          placeholder="Conte com detalhe o que está acontecendo"
+        />
+
+        <footer className="flex gap-2 mt-2">
+          <button
+            type="button"
+            className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+          >
+            <Camera className="w-6 h-6 text-zinc-100" />
+          </button>
+          <button
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+          >
+            Enviar feedback
+          </button>
+        </footer>
+      </form>
     </>
   );
 }
